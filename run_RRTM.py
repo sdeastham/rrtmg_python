4912d04021306ef6d8864bf4d403ae2d067051e9
Sebastian D. Eastham <seastham@mit.edu>
2023-11-23 15:32:32 -0500

    Implemented batch-based processing

--- run_RRTM.py
+++ run_RRTM.py
@@ -6,6 +6,70 @@ import sys
 import subprocess
 from datetime import datetime, timedelta
 
+def run_multi(dirname, sw_input_vec, lw_input_vec, cld_input_vec, sw_bin, lw_bin,
+              sw_output_short_vec,lw_output_short_vec,verbose=False):
+
+    bash_cmd_vec = ['#!/bin/sh']
+    sw_out_vec = []
+    lw_out_vec = []
+    for sw_input, lw_input, cld_input, sw_out, lw_out in zip(sw_input_vec,lw_input_vec,cld_input_vec,
+                                                             sw_output_short_vec,lw_output_short_vec):
+    
+        # Commands to prep for the run
+        sw_prep = 'unlink INPUT_RRTM; unlink IN_CLD_RRTM; ln -s {:s} INPUT_RRTM; ln -s {:s} IN_CLD_RRTM'.format(sw_input, cld_input)
+        lw_prep = 'unlink INPUT_RRTM; unlink IN_CLD_RRTM; ln -s {:s} INPUT_RRTM; ln -s {:s} IN_CLD_RRTM'.format(lw_input, cld_input)
+
+        # Build the bash commands for each run
+        sw_bash = '( echo {:s}; echo {:s}; echo "_sw_apcemm"; echo "./"; echo 2 ) | {:s}'.format(sw_input,cld_input,sw_bin)
+        lw_bash = '( echo {:s}; echo {:s}; echo "_lw_apcemm"; echo "./"; echo 2 ) | {:s}'.format(lw_input,cld_input,lw_bin)
+
+        # Commands to move data after generation
+        lw_move = 'mv OUTPUT-RRTM_lw_apcemm ' + lw_out
+        sw_move = 'mv OUTPUT_RRTM ' + sw_out
+        
+        LW_output_path = os.path.join(dirname,lw_out)
+        SW_output_path = os.path.join(dirname,sw_out)
+
+        sw_out_vec.append(SW_output_path)
+        lw_out_vec.append(LW_output_path)
+
+        for cmd in [sw_prep, sw_bash, sw_move, lw_prep, lw_bash, lw_move]:
+            bash_cmd_vec.append(cmd)
+
+    bash_cmd = '\n'.join(bash_cmd_vec)
+    
+    # Go to the directory containing the data. Easiest way to handle the fact that the SW code reads from
+    # INPUT_RRTM regardless of what file is specified as input
+    start_dir = os.getcwd()
+    try:
+        os.chdir(dirname)
+        
+        # Write the command list to file
+        f_batch = 'batch_rrtm.sh'
+        if os.path.isfile(f_batch):
+            os.remove(f_batch)
+        with open(f_batch,'w') as f:
+            for l in bash_cmd_vec:
+                f.write(f"{l}\n")
+        
+        # Owner rwx, everyone else r only
+        # Leading 0o means octal
+        os.chmod(f_batch,0o744)
+        #with open(f_batch,'r') as f:
+        #    print(f.read())
+        #print(os.getcwd())
+        
+        process = subprocess.Popen("./{:s}".format(f_batch), stdin=subprocess.PIPE, stdout=subprocess.PIPE, 
+                                   stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        if verbose:
+            print(output.decode())
+            print(error.decode())
+        
+    finally:
+        os.chdir(start_dir)
+    return lw_out_vec, sw_out_vec
+
 def run_single(dirname, sw_input, lw_input, cld_input, sw_bin, lw_bin,
                sw_output_short='sw_output',lw_output_short='lw_output',verbose=False):
     # Build the bash commands for each
@@ -59,7 +123,7 @@ def run_single(dirname, sw_input, lw_input, cld_input, sw_bin, lw_bin,
         os.chdir(start_dir)
     return LW_output_path, SW_output_path
 
-def run_directory(dirname, sw_bin=None, lw_bin=None, verbose=False):
+def run_directory(dirname, sw_bin=None, lw_bin=None, verbose=False, use_single=False):
 
     if sw_bin is None:
         sw_bin = './rrtmg_sw'
@@ -70,7 +134,14 @@ def run_directory(dirname, sw_bin=None, lw_bin=None, verbose=False):
     # Figure out how many cases we have
     all_sw_input = [x for x in os.listdir(dirname) if x.startswith('sw_input_t') and not os.path.isdir(x)
                                                       and x.endswith('_clr')]
-
+    print(len(all_sw_input))
+    if not use_single:
+        sw_out_vec = []
+        lw_out_vec = []
+        sw_in_vec = []
+        lw_in_vec = []
+        cld_in_vec = []
+        
     for sw_clr in all_sw_input:
         f_split = sw_clr.split('_')
         tstamp  = f_split[2]
@@ -82,9 +153,21 @@ def run_directory(dirname, sw_bin=None, lw_bin=None, verbose=False):
             sw_in  = 'sw_input_{:s}_{:s}_{:s}'.format(tstamp,column,cld_case)
             lw_in  = 'lw_input_{:s}_{:s}_{:s}'.format(tstamp,column,cld_case)
             cld_in = 'cld_input_{:s}_{:s}_{:s}'.format(tstamp,column,cld_case)
-            LW_out, SW_out = run_single(dirname,sw_in,lw_in,cld_in,
-                                        sw_bin,lw_bin,sw_out,lw_out,
-                                        verbose=verbose)
+            if use_single:
+                run_single(dirname,sw_in,lw_in,cld_in,
+                           sw_bin,lw_bin,sw_out,lw_out,
+                                 verbose=verbose)
+            else:
+                sw_out_vec.append(sw_out)
+                lw_out_vec.append(lw_out)
+                sw_in_vec.append(sw_in)
+                lw_in_vec.append(lw_in)
+                cld_in_vec.append(cld_in)
+                
+    if not use_single:
+        run_multi(dirname, sw_in_vec, lw_in_vec, cld_in_vec, sw_bin, lw_bin,
+              sw_out_vec,lw_out_vec,verbose=verbose)
+        
     return len(all_sw_input)
 
 if __name__ == '__main__':
